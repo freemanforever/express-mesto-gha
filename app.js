f2c36dab82e508669a295f37c4f0d1d515fad234
DmitriyL <dima-leg@yandex.ru>
2022-07-10 23:40:58 +0400

    feat skelet

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const _id = 3000;
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const router = require('./routes/users');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(router);
+
+app.listen(PORT);
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/users', (req, res) => {
+
+});
+router.get('/users/:userId', (req, res) => {
+
+});
+
+module.exports(router);
